00e7c8603a893393e77ade2f2456ad4fd415e73a
gitkyo <p.delaunay1@gmail.com>
2017-08-03 15:50:57 +0200

    add link to species in HP

--- wp-content/themes/nsobspheno/templates/content-page-accueil.php
+++ wp-content/themes/nsobspheno/templates/content-page-accueil.php
@@ -1,33 +1,37 @@
-<?php the_content(); ?>
+
+<?php
+	// displaying content written in the BO for the front page
+	the_content();
+?>
 
 
 
 
 <?php
 
-	// Display custom content type (inspiration : http://www.wpbeginner.com/wp-tutorials/how-to-create-custom-post-types-in-wordpress/)
+	/*
+	*  Display custom content type (inspiration : http://www.wpbeginner.com/wp-tutorials/how-to-create-custom-post-types-in-wordpress/)
+	*/
 
-	$args = array( 'post_type' => 'taxon', 'posts_per_page' => 50 );
+	// Set query
+	$args = array( 'post_type' => 'taxon', 'posts_per_page' => -1 );
 	$the_query = new WP_Query( $args );
+
 ?>
+
 <?php if ( $the_query->have_posts() ) : ?>
 	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-		<h2><?php the_title(); ?></h2>
+		<a href="<?php the_permalink(); ?>">
+			<h2><?php the_title(); ?></h2>
+		</a>
 
-		<!-- <div class="entry-content">
-			<?php // the_content(); ?>
-		</div>
+	<?php endwhile; ?>
 
-		<?php // wp_reset_postdata(); ?> -->
 
-		<?php endwhile; ?>
-
-<?php else: ?>
-	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
 
 
-<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+<?php //wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
 
 
