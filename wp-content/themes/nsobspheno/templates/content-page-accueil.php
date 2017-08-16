4ba1d864711f66c2bddb44d1ada1bee251880418
gitkyo <p.delaunay1@gmail.com>
2017-08-16 12:56:38 +0200

    Init loop of article post type in front page

--- wp-content/themes/nsobspheno/templates/content-page-accueil.php
+++ wp-content/themes/nsobspheno/templates/content-page-accueil.php
@@ -7,35 +7,38 @@
 	<?php
 		// displaying content written in the BO for the front page
 		the_content();
-	?>
 
-	<?php
+		// Csutom function to use later in template
+		function excerpt_truncate($excerpt, $limit) {
+		  $excerpt = explode(' ', $excerpt, $limit);
+		  if (count($excerpt)>=$limit) {
+		    array_pop($excerpt);
+		    $excerpt = implode(" ",$excerpt).'...';
+		  } else {
+		    $excerpt = implode(" ",$excerpt);
+		  }
+		  $excerpt = preg_replace('`[[^]]*]`','',$excerpt);
+		  return $excerpt;
+		}
 
 		/*
 		*  Display custom content type (inspiration : http://www.wpbeginner.com/wp-tutorials/how-to-create-custom-post-types-in-wordpress/)
 		*/
 
-
-		// Get current month
-
 		// Set query
 		$the_query = new WP_Query( array(
-		    'post_type' => 'taxon',
-		    'posts_per_page' => -1,
-		    'tax_query' => array(
-		        array (
-		            'taxonomy' => 'saisons',
-		            'field' => 'slug',
-		            'terms' => 'janvier',
-		            //'field' => 'term_id',
-		            //'terms' => 26,
-		        )
-		    ),
+			'post_type' => 'taxon',
+			'posts_per_page' => -1,
+			'tax_query' => array(
+				array (
+					'taxonomy' => 'saisons',
+					'field' => 'slug',
+					'terms' => 'janvier',
+					//'field' => 'term_id',
+					//'terms' => 26,
+				)
+			),
 		) );
-
-		/*$args = array( 'post_type' => 'taxon', 'posts_per_page' => -1 );
-		$the_query = new WP_Query( $args );*/
-
 
 	?>
 
@@ -123,6 +126,75 @@
 
 	<div class="siderBarRight actu">
 		<h2>Actualités</h2>
+
+		<?php
+
+			/*
+			*  Display article post type
+			*/
+
+			// Set query
+			$the_query = new WP_Query( array(
+			    'post_type' => 'post',
+			    'posts_per_page' => 5
+			    /*
+			    ,
+			    'tax_query' => array(
+			        array (
+			            'taxonomy' => 'saisons',
+			            'field' => 'slug',
+			            'terms' => 'janvier',
+			            //'field' => 'term_id',
+			            //'terms' => 26,
+			        )
+			    ),
+			    */
+			) );
+
+			if ( $the_query->have_posts() ){
+
+				while ( $the_query->have_posts() ) {
+
+					$the_query->the_post();
+
+					$post_id = get_the_ID();
+					$date_post = get_the_date();
+					$title_post = get_the_title($post_id);
+					$link_post = get_post_permalink($post_id);
+					$thumb_post =  get_the_post_thumbnail( $post_id, 'medium', array( 'class' => '' ) );
+
+					$excerpt = get_the_excerpt();
+					$excerpt_post_truncate = excerpt_truncate($excerpt, 20);
+
+
+
+
+					echo "
+					<a href='$link_post' class='oneArticle' title='$title_post' alt=''>
+
+						<div class='left'>$thumb_post</div>
+						<div class='right'>
+							<span class='date_post'>$date_post</span>
+							<span class='title_post'>$title_post</span>
+							<span class='excerpt_post'>$excerpt_post_truncate</span>
+						</div>
+
+					</a>";
+
+
+
+
+
+				}
+
+			}
+
+
+		?>
+
+
 	</div>
 
 </div>
+
+
